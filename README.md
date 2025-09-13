308fb0d506f1fe0e5e43b9f5fc14356915be11b6
roma-yac <roma_yac@ukr.net>
2025-09-13 17:26:36 +0300

    Add f-strings.py

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # f-strings
 
 Згадай про f-strings
+
+{var:[width][.precision][type]}
+
+type = d/f
---
+++ f-strings.py
@@ -0,0 +1,22 @@
+age = 20
+python_version = 3.13
+
+print(f"Your age is {age}")
+print(f"{python_version=}")
+
+number = 200
+print(f"{number:^8}$")
+
+x = 20.12384574378563456394785349875
+print(f"{x:.5f}")
+
+year = 2025
+month = 9
+day = 5
+print(f"{day:0>2}.{month:0>2}.{year}")
+
+p = 20.123
+print(f"{p:.1f}%")
+
+m = 100_000_000
+print(f"{m:,}")
